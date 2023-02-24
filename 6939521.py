c457ae14976e50e35f54adfc422f2c755f5a1d5e
dskatsi <125257562+dskatsi@users.noreply.github.com>
2023-02-24 14:32:47 +0000

    Add files via upload

---
+++ 6939521.py
@@ -0,0 +1,75 @@
+#this script gives details about cars and their prices 
+import sys
+hello=input('hello welcome to TAKE THE WHEEL LORD VENTURES, if you want to proceed input yes: ')
+if hello == 'yes' :
+    print('welcome once again')
+else :
+    sys.exit(0) #at this point
+results=input('which type of car do you prefer?: ')
+if results == str('Tesla Cybertruck') :
+    print('the price is five million dollars. thank you.')
+if results == str('BMW X2') :
+    print('the price is nine million dollars. thank you.')
+if results == str('Ferrari 488 Pista') :
+    print('the price is seven million dollars. thank you.')
+if results == str('Ford Explorer 2020') :
+    print('the price is ten million dollars. thank you.')
+if results == str('Porsche 718 Boxter') :
+    print('the price is twenty million dollars. thank you.')
+if results == str('Honda WR-V') :
+    print('the price is twelve million dollars. thank you.')
+if results == 'Lamborghini Aventador' :
+    print('the price is hundred million dollars. thank you.')
+if results == 'Toyota Yaris' :
+    print('the price is thirty thousand dollars. thank you.')
+if results == 'Bentley Bentayga' :
+    print('the price is six thousand dollars. thank you.')
+if results == 'Maserati Ghibil' :
+    print('the price is eight thousand dollars. thank you.')
+if results == 'Audi Suv' :
+    print('the price is fifty thousand dollars. thank you.')
+if results == 'Jeep Compass' :
+    print('the price is sixty thousand dollars. thank you.')
+if results == 'Subaru Impreza' :
+    print('the price is six thousand dollars. thank you.')
+if results == 'Chrysler Sebring' :
+    print('the price is twenty thousand dollars. thank you.')
+if results == 'Dodge Challenger' :
+    print('the price is ninety thousand dollars. thank you.')
+if results == 'Jaguar F-Type' :
+    print('the price is two thousand dollars. thank you.')
+if results == 'Bugatti Chiron' :
+    print('the price is seven million dollars. thank you.')
+if results == 'Lexus Ls' :
+    print('the price is eighty thousand dollars. thank you.')
+if results == 'Rolls Royce Dawn' :
+    print('the price is two million dollars. thank you.')
+if results == 'Acura RDX' :
+    print('the price is twelve thousand dollars. thank you.')
+if results == 'Aston Martin' :
+    print('the price is eleven thousand dollars. thank you.')
+if results == 'Volkswagen' :
+    print('the price is four thousand dollars. thank you.')
+if results == 'McLaren' :
+    print('the price is fourty million dollars. thank you.')
+if results == 'GMC' :
+    print('the price is fifty million dollars. thank you.')
+if results == 'Lincoln' :
+    print('the price is three hundred million dollars. thank you.')
+if results == 'Peugeot' :
+    print('the price is hundred million dollars. thank you.')
+if results == 'Saab' :
+    print('the price is seventy million dollars. thank you.')
+if results == 'Genesis' :
+    print('the price is two hundred million dollars. thank you.')
+if results == 'Suzuki' :
+    print('the price is two hundred million dollars. thank you.')
+if results == 'Fiat' :
+    print('the price is four million dollars. thank you.')
+else :
+    sys.exit(0)
+    
+    
+    #github repository link:https://github.com/dskatsi/Data-Structures-.git
+    #github name:dskatsi
+    
